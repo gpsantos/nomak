39b94615cae2c6c39ff03374f9d50bde0ec82826
Margareth <mbpolesi@gmail.com>
2013-06-16 22:17:03 -0400

    google geolocation, autocomplete para face da montanha e clube de montanhismo

--- js/cadastro.js
+++ js/cadastro.js
@@ -1,17 +1,51 @@
-$( document ).ready(function() {
+$(document).ready(function() {
 	$(".spinner").spinner();
-	
-	var facemontanhaUrl = 'json/listafacemontanha.json';
-	$.getJSON(facemontanhaUrl, function(jsonFaceMontanhaList) {
-		
-		var availableFaces = [];
-	
-		$.each(jsonFaceMontanhaList, function(i, montanha) {
-			availableFaces.push(montanha.nome);
-		});
-		$("#faceMontanha").autocomplete({
-			source: availableFaces
-		}); 
 
-	}).fail(function(jqxhr, textStatus, error) { alert(textStatus + ' ' + error);});
+	function loadAutoCompleteFromJSON(urlToAccessJSON, inputId) {
+		$.getJSON(urlToAccessJSON, function(jsonResultList) {
+
+			var availableItems = [];
+
+			$.each(jsonResultList, function(i, item) {
+				availableItems.push(item.sigla + " - " + item.nome);
+			});
+
+			$(inputId).autocomplete({
+				source : availableItems
+			});
+
+		}).fail(function(jqxhr, textStatus, error) {
+			alert(textStatus + ' ' + error);
+		});
+	}
+
+	var facemontanhaUrl = 'json/listafacemontanha.json';
+	loadAutoCompleteFromJSON(facemontanhaUrl, "#faceMontanha");
+
+	var clubemontanhismoUrl = 'json/listaclubemontanhismo.json';
+	loadAutoCompleteFromJSON(clubemontanhismoUrl, "#clube");
+
+	var googlemapsUrl = 'json/googlemaps.json';
+
+	function loadGoogleGeoLocation() {
+		if ($("#latitude").val() != "" && $("#longitude").val() != "") {
+			$.getJSON(googlemapsUrl + "?latitude=" + $("#latitude").val() + "&longitude=" + $("#longitude").val(), function(data) {
+				if (data.status == "OK")
+					var c, e;
+				$.each(data.results[0].address_components, function(i, address_component) {
+					$.each(address_component.types, function(j, type) {
+						if (type == "locality")
+							c = address_component.long_name;
+						if (type == "administrative_area_level_1")
+							e = address_component.short_name;
+					});
+				});
+				$("#cidade").val(c + " - " + e);
+			});
+		}
+	}
+
+	$("#latitude").blur(loadGoogleGeoLocation);
+	$("#longitude").blur(loadGoogleGeoLocation);
+
 });
